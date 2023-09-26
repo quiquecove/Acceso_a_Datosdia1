178975f16caef104ed661530305f1acda046e13e
quiquecove <quiquehuertalopez@gmail.com>
2023-09-26 19:37:08 +0200

    bufferreader y writer

--- src/quijote2/quijoteBufferReaderWriter1.java
+++ src/quijote2/quijoteBufferReaderWriter1.java
@@ -13,41 +13,41 @@ public class quijoteBufferReaderWriter1 {
 
 		final long tInicio = System.currentTimeMillis();
 		
-//		File file = new File("QuijoteCompleto.txt");
-//
-//		FileReader reader = new FileReader(file);
-//		FileWriter writter = new FileWriter("Quijote3");
-//
-//		BufferedReader lector = new BufferedReader(reader);
-//		BufferedWriter escritor = new BufferedWriter(writter);
-//
-//		String linea;
-//		while ((linea = lector.readLine()) != null) {
-//			escritor.write(linea + "\n");
-//		}
-//		while ((reader.read()) != -1) {
-//			escritor.write(lector.readLine() + "\n");
-//		}
-//		escritor.close();
-//		lector.close();
+		File file = new File("QuijoteCompleto.txt");
+
+		FileReader reader = new FileReader(file);
+		FileWriter writter = new FileWriter("Quijote3");
+
+		BufferedReader lector = new BufferedReader(reader);
+		BufferedWriter escritor = new BufferedWriter(writter);
+
+		String linea;
+		while ((linea = lector.readLine()) != null) {
+			escritor.write(linea + "\n");
+		}
+		while ((reader.read()) != -1) {
+			escritor.write(lector.readLine() + "\n");
+		}
+		escritor.close();
+		lector.close();
 
 		// pruebas sin buffer
 
-		File fl = new File("QuijoteCompleto.txt");
-
-		FileReader fr = new FileReader(fl);
-		FileWriter fw = new FileWriter("Quijote3.txt");
-		int aux = 0;
-
-		do {
-
-			aux = fr.read();
-			fw.write(aux);
-
-		} while (aux != -1);
-
-		fr.close();
-		fw.close();
+//		File fl = new File("QuijoteCompleto.txt");
+//
+//		FileReader fr = new FileReader(fl);
+//		FileWriter fw = new FileWriter("Quijote3.txt");
+//		int aux = 0;
+//
+//		do {
+//
+//			aux = fr.read();
+//			fw.write(aux);
+//
+//		} while (aux != -1);
+//
+//		fr.close();
+//		fw.close();
 
 		final long tFinal = System.currentTimeMillis();
 		long tTotal = tFinal - tInicio;
